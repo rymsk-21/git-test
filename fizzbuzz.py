9d0fb24e420641be6f748d1b260b5e0732df72a3
atsuya-kose <kose@peoplytics.work>
2021-12-03 09:45:14 +0900

    fizzbuzz test

---
+++ fizzbuzz.py
@@ -0,0 +1,6 @@
+class FizzBuzz:
+    def __init__(self):
+        pass
+
+    def fizznbuzz(num):
+        return 1
---
+++ test_fizzbuzz.py
@@ -0,0 +1,5 @@
+import pytest
+from fizzbuzz import FizzBuzz
+
+def test_one():
+    assert FizzBuzz.fizznbuzz(1) == 1
